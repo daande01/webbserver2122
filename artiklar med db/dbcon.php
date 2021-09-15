58e14de9b61d1194e156d43e2af0a66e221fd431
daande1 <danielevertandersson>
2021-09-15 12:23:59 +0200

    new file

---
+++ artiklar med db/dbcon.php
@@ -0,0 +1,31 @@
+<?php
+
+class dbconnect {
+
+public $pdo;
+
+  public function __construct(){
+
+    $host = '127.0.0.1';
+    $db   = 'temp2122';
+    $user = 'temp';
+    $pass = 'temp';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+      PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    try {
+      $this->pdo = new PDO($dsn, $user, $pass, $options);
+    } catch (\PDOException $e) {
+      echo 'Connection failed: ' . $e->getMessage();
+    }
+
+
+  }
+}
+
+    ?>
