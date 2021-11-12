94d6cba9e6e88647f7c970a6a54cc085b177b5ca
daande1 <danielevertandersson>
2021-11-12 09:28:55 +0100

    new

--- angeltest/get.php
+++ angeltest/get.php
@@ -3,10 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-  $username  =filter_input(INPUT_GET, 'name',FILTER_SANITIZE_SPECIAL_CHARS);
-  echo $username;
-
-  
 require_once('dbconnect.php');
 $db=new dbconnect();
 
